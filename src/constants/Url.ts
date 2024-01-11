bdc1c2e55bae4668609a85eb54881d2f6080b40d
synguyen <nghsynguyen98@gmail.com>
2024-01-12 00:57:26 +0700

    add env variables

--- src/constants/Url.ts
+++ src/constants/Url.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://chika-memoria.azurewebsites.net';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const OAUTH2_REDIRECT_URI = `${window.location.origin}/oauth2/redirect`;
 export const GOOGLE_AUTH_URL = `${BACKEND_URL}/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`;
 
