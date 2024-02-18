33c37e14e9ee6168de1a0422187e8a116dc016a9
synguyen <nghsynguyen98@gmail.com>
2024-02-18 22:59:05 +0700

    add new components

---
+++ src/pages/about-memoria/AboutMemoria.tsx
@@ -0,0 +1,10 @@
+import "./AboutMemoria.scss";
+
+export default function AboutMemoria() {
+
+    return (
+        <section className="memoria-container">
+            Memoria là gì
+        </section>
+    )
+}
