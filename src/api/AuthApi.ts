efe63ffb5dcb784c570419d403d19793b41ff5d0
synguyen <nghsynguyen98@gmail.com>
2024-08-05 00:04:21 +0700

    update project

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -4,10 +4,6 @@ const API_URL = '/memoria/microsoft';
 
 export class AuthApi {
 
-    static async getMicrosoftTokenAvailable(): Promise<any> {
-        return appAxios.get(`${API_URL}/token`).then(res => res.data);
-    }
-
     static async sendAuthorizeCode(code: string): Promise<void> {
         return appAxios.get(`${API_URL}/oauth2?code=${code}`).then();
     }
