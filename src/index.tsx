88759278c84886c2d7de68fe759f30363aba07f6
synguyen <nghsynguyen98@gmail.com>
2024-10-20 00:52:06 +0700

    Update queryClient

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,13 @@ import {Provider} from "react-redux";
 import store from './app/store';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1
+        }
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
