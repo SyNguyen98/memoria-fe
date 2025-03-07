4904b3d4b681d50fd134207586610ebe36b07b75
synguyen <nghsynguyen98@gmail.com>
2025-03-08 00:17:22 +0700

    Setup unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        // this points to the setup file that we created earlier
+        setupFiles: "./src/setupTests.ts",
+        // you might want to disable the `css: true` line, since we don't have
+        // tests that rely on CSS -- and parsing CSS is slow.
+        // I'm leaving it in here because often people want to parse CSS in tests.
+        css: true,
+    },
 })
