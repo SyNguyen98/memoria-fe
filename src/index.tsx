4ab5ee9b9fec3173c88c081bc1323d4b26cdb1e3
synguyen <nghsynguyen98@gmail.com>
2025-05-04 01:28:55 +0700

    Wrap App component with AudioProvider for improved audio state management

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from './app/App';
 import {Provider} from "react-redux";
 import store from './app/store';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import AudioProvider from "./providers/AudioProvider.tsx";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -19,9 +20,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <Provider store={store}>
-                <BrowserRouter basename={import.meta.env.VITE_BASENAME}>
-                    <App/>
-                </BrowserRouter>
+                <AudioProvider>
+                    <BrowserRouter basename={import.meta.env.VITE_BASENAME}>
+                        <App/>
+                    </BrowserRouter>
+                </AudioProvider>
             </Provider>
         </QueryClientProvider>
     </React.StrictMode>
