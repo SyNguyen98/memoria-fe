763fc055d3710fd2a843b26ae26a72b9f53c51b2
synguyen <nghsynguyen98@gmail.com>
2025-07-28 21:53:49 +0700

    feat: add 'no_item' message and styling to ItemList component

--- src/pages/item/ItemList.tsx
+++ src/pages/item/ItemList.tsx
@@ -74,7 +74,9 @@ export default function ItemList() {
                 </Typography>
             }
             {/* Image/Video List */}
-            {itemQuery.isLoading ? <AppLoader/> : (
+            {itemQuery.isLoading && <AppLoader/>}
+
+            {items.length > 0 ? (
                 <Grid className="item-list" container spacing={1}>
                     {items.map((item, index) =>
                         <Grid key={item.id} size={{xs: 4, sm: 3, md: 3, lg: 2}}
@@ -84,6 +86,10 @@ export default function ItemList() {
                         </Grid>
                     )}
                 </Grid>
+            ) : (
+                <Typography variant="body1" className="no-item-text">
+                    {t("item.no_item")}
+                </Typography>
             )}
 
             {/* Dialogs */}
