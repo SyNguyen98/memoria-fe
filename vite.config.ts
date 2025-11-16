73781a4cfa2973f462ba92dda1590bb079e90ea2
synguyen <nghsynguyen98@gmail.com>
2025-11-16 20:53:42 +0700

    add path aliases for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@
 
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+          "@constants": path.join(__dirname, "src/constants/"),
+          "@models": path.join(__dirname, "src/models/"),
+          "@queries": path.join(__dirname, "src/queries/"),
+          "@utils": path.join(__dirname, "src/utils/"),
+      }
+    },
     test: {
         globals: true,
         environment: "jsdom",
